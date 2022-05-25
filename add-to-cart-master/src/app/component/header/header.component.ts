3ad63b5bdbe56b2651bbbe34b63d991c09a20ce3
sumit7908 <sumitslgsmc@gmail.com>
2022-05-25 21:51:52 +0530

    Inisial commite

---
+++ add-to-cart-master/src/app/component/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  public totalItem : number = 0;
+  public searchTerm !: string;
+  constructor(private cartService : CartService) { }
+
+  ngOnInit(): void {
+   
+  }
+  search(event:any){
+    this.searchTerm = (event.target as HTMLInputElement).value;
+    console.log(this.searchTerm);
+    this.cartService.search.next(this.searchTerm);
+  }
+}
